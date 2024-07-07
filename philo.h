7266c0a3c80d8eb2c572988218a7746bc9fe48a2
reduno96 <reduno96@gmail.com>
2024-07-07 14:20:32 +0000

    Refactor code to improve time-related functions

--- philo.h
+++ philo.h
@@ -4,34 +4,43 @@
 # include <pthread.h>
 # include <stdio.h>
 # include <stdlib.h>
+# include <sys/time.h>
 # include <unistd.h>
 
-typedef struct s_check
+typedef struct s_info
 {
-	int			num_of_philo;
-	int			time_to_die;
-	int			time_to_eat;
-	int			philo_eat_limit;
-	int			time_to_sleep;
-}				t_check;
+	int				num_of_philo;
+	int				time_to_die;
+	int				time_to_eat;
+	int				philo_eat_limit;
+	int				time_to_sleep;
+	int				end;
+	t_fork			fork;
+}					t_info;
+
+typedef struct s_fork
+{
+	int				left_fork;
+	int				right_fork;
+	pthread_mutex_t	mutex_fork;
+}					t_fork;
 
 typedef struct s_philosopher
 {
-	pthread_t	theard;
+	pthread_t		theard;
+	int				id;
+	t_info			*data;
+	long long		creation_time;
+	int				think;
+	int				eating;
+	size_t			last_meal;
+}					t_philosopher;
 
-	int			id;
-	int			left_fork;
-	int			right_fork;
-	t_check		data;
-	int			think;
-	int			eat;
-}				t_philosopher;
-
-
-void			ft_put_error(char *s);
-void			thinking(t_philosopher *philosopher);
-void			eating(t_philosopher *philosopher);
-void			sleeping(t_philosopher *philosopher);
-int				ft_atoi(const char *str);
+void				ft_put_error(char *s);
+void				thinking(t_philosopher *philosopher);
+void				eating(t_philosopher *philosopher);
+void				sleeping(t_philosopher *philosopher);
+int					ft_atoi(const char *str);
+long long			ft_get_time(void);
 
 #endif

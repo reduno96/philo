ab7f6514131f4c92ec496fa08a6e34ef3e5239a0
reduno96 <reduno96@gmail.com>
2024-07-08 10:28:14 +0000

    Update eating function to use mutexes for fork locking

--- ft_state.c
+++ ft_state.c
@@ -6,7 +6,7 @@
 /*   By: rel-mora <reduno96@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 08:49:18 by rel-mora          #+#    #+#             */
-/*   Updated: 2024/07/07 18:18:25 by rel-mora         ###   ########.fr       */
+/*   Updated: 2024/07/08 10:27:44 by rel-mora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,24 +18,25 @@ void	ft_print_actions(t_philosopher *philo, char action)
 		printf("\e[4;36m Philosopher %d is thinking.\n", philo->id);
 }
 
-void	thinking(t_philosopher *philo)
-{
-	ft_print_actions(philo, 'T');
-}
-
 void	eating(t_philosopher *philo)
 {
-	pthread_mutex_lock(&philo->data->fork.mutex_left_f);
-	pthread_mutex_lock(&philo->data->fork.mutex_right_f);
-	printf("\e[0;32m Philosopher %d has taken a fork right is %d .\n",
-		philo->id, philo->data->fork.right_fork);
-	printf("\e[1;33m Philosopher %d has taken a fork left is %d .\n", philo->id,
-		philo->data->fork.left_fork);
-	philo->eating++;
-	printf(" \033[0;91mPhilosopher %d is a eating.\n", philo->id);
-	pthread_mutex_unlock(&philo->data->fork.mutex_right_f);
-	pthread_mutex_unlock(&philo->data->fork.mutex_left_f);
-	printf("\e[0;34m Philosopher %d has finished eating.\n", philo->id);
+	pthread_mutex_lock(&philo->data->forks[philo->j].mutex);
+	pthread_mutex_lock(&philo->data->forks[(philo->j + 1)
+		% philo->data->num_of_philo].mutex);
+	printf("\e[1;33m Philosopher %d has taken a fork right is %d .\n",
+		philo->id, philo->data->forks[philo->j].right_fork);
+	printf("\e[1;35m Philosopher %d has taken a fork left is %d .\n", philo->id,
+		philo->data->forks[(philo->j + 1)
+		% philo->data->num_of_philo].right_fork);
+	pthread_mutex_unlock(&philo->data->forks[philo->j].mutex);
+	pthread_mutex_unlock(&philo->data->forks[(philo->j + 1)
+		% philo->data->num_of_philo].mutex);
+	// printf("\e[1;33m Philosopher %d has taken a fork left is %d .\n",
+	//	philo->id,
+	// 	philo->data->fork.left_fork);
+	// philo->eating++;
+	// printf(" \033[0;91mPhilosopher %d is a eating.\n", philo->id);
+	// printf("\e[0;34m Philosopher %d has finished eating.\n", philo->id);
 	// philo->last_meal = ft_get_time();
 	// if (ft_time_passed(philo->last_meal) <= philo->data->time_to_eat)
 	// {
@@ -43,7 +44,13 @@ void	eating(t_philosopher *philo)
 	// 	philo->data->end = 0;
 	// 	return ;
 	// }
+	printf("------------------\n");
 	usleep(philo->data->time_to_eat * 1000);
+}
+
+void	thinking(t_philosopher *philo)
+{
+	ft_print_actions(philo, 'T');
 }
 
 void	sleeping(t_philosopher *philo)
